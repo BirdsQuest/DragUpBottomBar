8cbdc3e959e68e13b28fee32daae1e5895f27ec0
Matt Bird <randomcode@gmail.com>
2017-03-03 03:54:50 +1300

    Basic View from https://github.com/miguelhincapie/CustomBottomSheetBehavior http://guides.codepath.com/android/Handling-Scrolls-with-CoordinatorLayout http://guides.codepath.com/android/Using-DialogFragment

--- app/src/main/java/com/birdsquest/dragup/DragUp.java
+++ app/src/main/java/com/birdsquest/dragup/DragUp.java
@@ -1,9 +1,32 @@
 package com.birdsquest.dragup;
 
-/**
- * Created by Matt Bird on 3/03/2017.
- */
+import android.content.Context;
+import android.support.design.widget.CoordinatorLayout;
+import android.util.AttributeSet;
 
-public class DragUp{
-	
+/**
+build.gradle~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ compile 'com.android.support:appcompat-v7:23.2.0'
+ compile 'com.android.support:design:23.2.0'
+*/
+
+public class DragUp extends CoordinatorLayout{
+
+	public DragUp(Context context){
+		super(context);
+		init(context, null, -1);
+	}
+
+	public DragUp(Context context, AttributeSet attrs){
+		super(context, attrs);
+		init(context, attrs, -1);
+	}
+
+	public DragUp(Context context, AttributeSet attrs, int defStyleAttr){
+		super(context, attrs, defStyleAttr);
+		init(context, attrs, defStyleAttr);
+	}
+	public void init(Context context, AttributeSet attrs, int defStyleAttr){
+
+	}
 }
